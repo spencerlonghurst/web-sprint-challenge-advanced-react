e0f38b1e8c2ec7c649492ac0060e7ac19eb493b4
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-03-11 13:34:11 -0700

    Can change state of left and right

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,7 +1,83 @@
 import React from 'react'
+import axios from 'axios';
+
+const URL = 'http://localhost:9000/api/result';
+
+const initialState = {
+  grid: {
+    x: 2,
+    y: 2,
+    steps: 0,
+    email: '' }
+}
 
 export default class AppClass extends React.Component {
+  constructor() {
+    super()
+    this.state = initialState
+  }
+
+  changeRight = () => {
+    this.setState({
+      ...this.state,
+      grid: { ...this.state.grid, x: this.state.grid.x + 1 }
+    })
+  }
+
+
+
+
+  changeLeft = () => {
+  this.setState({
+    ...this.state, 
+    grid: { ...this.state.grid, x: this.state.grid.x - 1 }
+  })
+  }
+
+
+
+  // const [x, y] = getCoordinates(grid)
+  //   console.log(`(${x}, ${y})`)
+
+//   onEmailFormSubmit = event => {
+//     event.preventDefault();
+//   }
+
+// postNewX = () => {
+//   axios.post(URL, )
+//   .then(res => {
+//     this.setState({
+//       ...this.state,
+//         grid: []
+//     })
+//   })
+  
+//   .catch(err => {
+//     debugger
+//   })
+// }
+// onXBtnSubmit = () => {
+//   this.postNewX();
+// }
+
+
+
+
+
+
+
+
+
+
+  componentDidMount() {
+   
+  }
+
   render() {
+    console.log('state is', this.state)
+
+
+
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
@@ -24,15 +100,15 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
+          <button id="left" onClick={this.changeLeft}>LEFT</button>
           <button id="up">UP</button>
-          <button id="right">RIGHT</button>
+          <button id="right" onClick={this.changeRight}>RIGHT</button>
           <button id="down">DOWN</button>
           <button id="reset">reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input id="submit" type="submit" onSubmit={this.onEmailFormSubmit}></input>
         </form>
       </div>
     )
