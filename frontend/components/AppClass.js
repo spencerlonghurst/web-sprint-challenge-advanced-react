91152a14a2c45b27166aeb1971f4b1d9202bd0c7
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-03-11 17:46:24 -0700

    added functionality of the buttons and started the submit button

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -7,8 +7,10 @@ const initialState = {
   grid: {
     x: 2,
     y: 2,
+  },
     steps: 0,
-    email: '' }
+    email: '',
+    errorMessage: '',
 }
 
 export default class AppClass extends React.Component {
@@ -20,72 +22,70 @@ export default class AppClass extends React.Component {
   changeRight = () => {
     this.setState({
       ...this.state,
-      grid: { ...this.state.grid, x: this.state.grid.x + 1 }
+      grid: { ...this.state.grid, x: this.state.grid.x + 1 },
+      steps: this.state.steps + 1 
     })
   }
 
   changeLeft = () => {
     this.setState({
       ...this.state, 
-      grid: { ...this.state.grid, x: this.state.grid.x - 1 }
+      grid: { ...this.state.grid, x: this.state.grid.x - 1 },
+      steps: this.state.steps + 1 
     })
     }
 
   changeDown = () => {
     this.setState({
       ...this.state,
-      grid: { ...this.state.grid, y: this.state.grid.y - 1 }
+      grid: { ...this.state.grid, y: this.state.grid.y + 1 },
+      steps: this.state.steps + 1 
     })
   }
 
   changeUp = () => {
     this.setState({
       ...this.state,
-      grid: { ...this.state.grid, y: this.state.grid.y + 1 }
+      grid: { ...this.state.grid, y: this.state.grid.y - 1 },
+      steps: this.state.steps + 1 
     })
   }
 
+  onToDoEmailChange = evt => {
+    const { value } = evt.target;
+    this.setState({ ...this.state, email: value })
+  }
 
  
 
-
-
-  // const [x, y] = getCoordinates(grid)
-  //   console.log(`(${x}, ${y})`)
-
-//   onEmailFormSubmit = event => {
-//     event.preventDefault();
-//   }
-
-// postNewX = () => {
-//   axios.post(URL, )
-//   .then(res => {
-//     this.setState({
-//       ...this.state,
-//         grid: []
-//     })
-//   })
-  
-//   .catch(err => {
-//     debugger
-//   })
-// }
-// onXBtnSubmit = () => {
-//   this.postNewX();
-// }
-
-
-
-
-
-
-
-
-
-
-  componentDidMount() {
-   
+  onSubmit = evt => {
+    evt.preventDefault()
+    axios.post(URL)
+    .then(res => {
+      this.setState({
+        ...this.state,
+        email: this.state.email
+      })
+    })
+    .catch(err => {
+      this.setState({ ...this.state, errorMessage: err.response.data.message })  
+    })
   }
+
+  // changeInput = (key, value) => {
+  //   console.log('STATE IS ABOUT TO CHANGE')
+  //   this.setState({
+  //     ...this.state,
+  //     form: { ...this.state.form, [key]: value },
+  //   })
+  // }
+
+
+
+
+
+
+
 
   render() {
     console.log('state is', this.state)
@@ -96,22 +96,22 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.grid.x}, {this.state.grid.y})</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          <div className={`square ${this.state.grid.x === 1 && this.state.grid.y === 1  ? 'active' : ''}`}>{`${this.state.grid.x === 1 && this.state.grid.y === 1  ? 'B' : ''}`}</div>
+          <div className={`square ${this.state.grid.x === 2 && this.state.grid.y === 1  ? 'active' : ''}`}>{`${this.state.grid.x === 2 && this.state.grid.y === 1  ? 'B' : ''}`}</div>
+          <div className={`square ${this.state.grid.x === 3 && this.state.grid.y === 1  ? 'active' : ''}`}>{`${this.state.grid.x === 3 && this.state.grid.y === 1  ? 'B' : ''}`}</div>
+          <div className={`square ${this.state.grid.x === 1 && this.state.grid.y === 2  ? 'active' : ''}`}>{`${this.state.grid.x === 1 && this.state.grid.y === 2  ? 'B' : ''}`}</div>
+          <div className={`square ${this.state.grid.x === 2 && this.state.grid.y === 2  ? 'active' : ''}`}>{`${this.state.grid.x === 2 && this.state.grid.y === 2  ? 'B' : ''}`}</div>
+          <div className={`square ${this.state.grid.x === 3 && this.state.grid.y === 2  ? 'active' : ''}`}>{`${this.state.grid.x === 3 && this.state.grid.y === 2  ? 'B' : ''}`}</div>
+          <div className={`square ${this.state.grid.x === 1 && this.state.grid.y === 3  ? 'active' : ''}`}>{`${this.state.grid.x === 1 && this.state.grid.y === 3  ? 'B' : ''}`}</div>
+          <div className={`square ${this.state.grid.x === 2 && this.state.grid.y === 3  ? 'active' : ''}`}>{`${this.state.grid.x === 2 && this.state.grid.y === 3  ? 'B' : ''}`}</div>
+          <div className={`square ${this.state.grid.x === 3 && this.state.grid.y === 3  ? 'active' : ''}`}>{`${this.state.grid.x === 3 && this.state.grid.y === 3  ? 'B' : ''}`}</div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.errorMessage}</h3>
         </div>
         <div id="keypad">
           <button id="left" onClick={this.changeLeft}>LEFT</button>
@@ -120,9 +120,9 @@ export default class AppClass extends React.Component {
           <button id="down" onClick={this.changeDown}>DOWN</button>
           <button id="reset">reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit" onSubmit={this.onEmailFormSubmit}></input>
+        <form onSubmit={this.onSubmit}>
+          <input id="email" type="email" placeholder="type email" onChange={this.onToDoEmailChange}></input>
+          <input id="submit" type="submit"></input>
         </form>
       </div>
     )
